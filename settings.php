0dae77fef9c57aa3906b95d1ea9fbf8688e6fdac
akkaoui <aakkaoui@groupe-comelli.com>
2020-07-07 12:02:21 +0200

    first commit

---
+++ settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Google App Client Id */
+define('CLIENT_ID', 'xxxxxxxxxxxxxxxx');
+
+/* Google App Client Secret */
+define('CLIENT_SECRET', 'xxxxxxxxxxxxxxxx');
+
+/* Google App Redirect Url */
+define('CLIENT_REDIRECT_URL', 'xxxxxxxxxxxxxxxx');
+
+?>
